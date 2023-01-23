68d1fec723cb68f3107c62f415495312acc66c6f
unknown <yassine.yzd@gmail.com>
2023-01-23 17:43:48 +0100

    Text Vectorization(v0.1)

--- Transformer Model NLP.py
+++ Transformer Model NLP.py
@@ -59,6 +59,23 @@ text_file = pathlib.Path(text_file).parent / "fra.txt"
 # Finally, the line is split into two parts at the tab character using the split() method.
 #  The second part is then wrapped in "[start]" and "[end]" before being returned along
 #  with the first part.
+
+# The following code is reading a text file and normalizing the lines in it. 
+# It then selects 5 random pairs from the normalized text and prints them. 
+# Then it saves the text pairs to a binary file called "text_pairs.pickle"
+#  using the python pickle module. The code then reads the "text_pairs.pickle" file
+#  and loads the text pairs into the variable "text_pairs".
+
+# Next, the code tokenizes the English and French sentences and counting the tokens in each.
+#  It also finds the maximum length of the sentences in tokens and prints the results.
+
+# Finally, the code plots a histogram of the sentence length (in tokens) for both English 
+# and French sentences. The y-axis is in log scale, and the histograms are plotted in red
+#  and blue, respectively. The code also plots the maximum sentence length for each 
+#  language using the same color as the corresponding histogram. The code also adds 
+#  a title and a legend to the plot and shows it.
+
+# It is also noted that sentence length fits Benford's law and can be plotted with the log scale.
 # =============================================================================
 
 import pickle
@@ -108,3 +125,109 @@ print(f"Total French tokens : {len(fra_tokens)}")
 print(f"Max English lengths : {eng_maxlen}")
 print(f"Max French lengths : {fra_maxlen}")
 print(f"{len(text_pairs)} total pairs")
+
+import matplotlib.pyplot as plt
+
+#histogram of sentence length intokens
+en_lengths = [len(eng.split()) for eng, fra in text_pairs]
+fr_lengths = [len(fra.split()) for eng, fra in text_pairs]
+
+plt.hist(en_lengths, label="en", color="red", alpha=0.33)
+plt.hist(fr_lengths, label="fr", color="blue", alpha=0.33)
+plt.yscale("log") #Sentence length fits Benford's law
+plt.ylim(plt.ylim()) # Make y-axis consistent for both plots
+plt.plot([max(en_lengths), max(en_lengths)], plt.ylim(), color="red")
+plt.plot([max(fr_lengths), max(fr_lengths)], plt.ylim(), color="blue")
+plt.legend()
+plt.title("Examples count vs Token length")
+plt.show()
+
+# =============================================================================
+"""Vectorization and making Dataset"""
+# =============================================================================
+from tensorflow.keras.layers import TextVectorization
+
+# Load normalized sentence pairs
+with open("text_pairs.pickle", "rb") as fp:
+    text_pairs = pickle.load(fp)
+    
+# train-test-val split of randomized sentence pairs 
+random.shuffle(text_pairs)
+n_val = int(0.15*len(text_pairs))
+n_train = len(text_pairs) - 2*n_val
+train_pairs = text_pairs[:n_train]
+val_pairs = text_pairs[n_train:n_train+n_val]
+test_pairs = text_pairs[n_train+n_val:]
+
+# Parameter determined after analyzing the input data
+vocab_size_en = 10000
+vocab_size_fr = 20000
+seq_length = 20
+
+# Create vectorizer
+eng_vectorizer = TextVectorization(
+    max_tokens = vocab_size_en,
+    standardize=None,
+    split="whitespace",
+    output_mode="int",
+    output_sequence_length=seq_length,
+    )
+
+fra_vectorizer = TextVectorization(
+    max_tokens = vocab_size_fr,
+    standardize=None,
+    split="whitespace",
+    output_mode="int",
+    output_sequence_length=seq_length,
+    )
+
+# train the vectorization layer using training dataset
+train_eng_texts = [pair[0] for pair in train_pairs]
+train_fra_texts = [pair[1] for pair in train_pairs]
+eng_vectorizer.adapt(train_eng_texts)
+fra_vectorizer.adapt(train_fra_texts)
+
+# save for subsequent steps
+with open("vectorize.pickle", "wb") as fp:
+    data = {
+        "train": train_pairs,
+        "val": val_pairs,
+        "test": test_pairs,
+        "engvec_config": eng_vectorizer.get_config(),
+        "engvec_weights": eng_vectorizer.get_weights(),
+        "fravec_config": fra_vectorizer.get_config(),
+        "fravec_weights": fra_vectorizer.get_weights(),
+        }
+    pickle.dump(data, fp)
+    
+# Load text data and vectorizer weights
+with open("victorize.pickle", "rb") as fp:
+    data = pickle.load(fp)
+    
+train_pairs = data["train"]
+val_pairs = data["val"]
+test_pairs = data["test"] # Not used
+
+eng_vectorizer = TextVectorization.from_config(data["engvec_config"])
+eng_vectorizer.set_weigths(data["engvec_weights"])
+fra_vectorizer = TextVectorization.from_config(data["fravec_config"])
+fra_vectorizer.set_weigths(data["fravec_weights"])
+
+# Set up dataset object
+def format_dataset(eng, fra):
+    eng = eng_vectorizer(eng)
+    fra = fra_vectorizer(fra)
+    source = {"encoder_inputs": eng,
+              "decoder_inputs": fra[:, :-1]}
+    target = fra[:, 1:]
+    return (source, target)
+
+def make_dataset(pairs, batch_size=64):
+    
+    # Aggregate sentences using zip(*pairs)
+    eng_texts, fra_texts = zip(*pairs)
+    
+    # Convert them into, and then create tensors
+    dataset = tf.data.Dataset.from_tensor_slices((list(eng_texts), list(fra_texts)))
+    return dataset.shuffle(2048).batch(batch_size).map(format_dataset) \
+                  .prefetch(16).cache()
