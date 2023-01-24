449329c75d6b43b43eb2606324e174af8d9b7cf4
unknown <yassine.yzd@gmail.com>
2023-01-24 23:55:02 +0100

    Text Vectorization(v0.2)

--- Transformer Model NLP.py
+++ Transformer Model NLP.py
@@ -6,7 +6,7 @@ Created on Mon Jan 23 12:11:58 2023
 """
 
 # ===========================================================================================
-# A transformer model is a type of neural network architecture used for natural             =
+#  A transformer model is a type of neural network architecture used for natural            =
 #  language processing tasks such as language translation, text summarization,              =
 #  and question answering. It was introduced in the 2017 paper "Attention Is All You Need"  =
 #  by Google researchers. The transformer uses self-attention mechanisms to weigh           =
@@ -26,15 +26,13 @@ import pathlib
 """Obtaining Data"""
 # =============================================================================
 # Downloading dataset
-text_file = tf.keras.utils.get_file(
-    fname='fra-eng.zip',
-    origin='http://storage.googleapis.com/download.tensorflow.org/data/fra-eng.zip',
-    extract=True,
-    )
 
-# Showing where the file is located now
+text_file = tf.keras.utils.get_file(
+    fname="fra-eng.zip",
+    origin="http://storage.googleapis.com/download.tensorflow.org/data/fra-eng.zip",
+    extract=True,
+)
 text_file = pathlib.Path(text_file).parent / "fra.txt"
-# print(text_file)
 
 # =============================================================================
 """Text Normalization"""
@@ -83,47 +81,45 @@ import random
 import re
 import unicodedata
 
+ 
 def normalize(line):
     """Normalize a line of text and split into two at the tab character"""
     line = unicodedata.normalize("NFKC", line.strip().lower())
-    line = re.sub(r"^([^ \w])(?!\s)",r"\1 ", line)
+    line = re.sub(r"^([^ \w])(?!\s)", r"\1 ", line)
     line = re.sub(r"(\s[^ \w])(?!\s)", r"\1 ", line)
-    line = re.sub(r"(?!\s)(^ \w)$", r" \1", line)
+    line = re.sub(r"(?!\s)([^ \w])$", r" \1", line)
     line = re.sub(r"(?!\s)([^ \w]\s)", r" \1", line)
     eng, fra = line.split("\t")
-    fra = "[start]" + fra + "[end]"
+    fra = "[start] " + fra + " [end]"
     return eng, fra
-
-#normalize each line and separate into English and French
+ 
+# normalize each line and separate into English and French
 with open(text_file) as fp:
     text_pairs = [normalize(line) for line in fp]
-    
-#print some samples
-
+ 
+# print some samples
 for _ in range(5):
     print(random.choice(text_pairs))
-
+ 
 with open("text_pairs.pickle", "wb") as fp:
     pickle.dump(text_pairs, fp)
 
 with open("text_pairs.pickle", "rb") as fp:
     text_pairs = pickle.load(fp)
-    
-#Count tokens
+ 
+# count tokens
 eng_tokens, fra_tokens = set(), set()
 eng_maxlen, fra_maxlen = 0, 0
-
 for eng, fra in text_pairs:
     eng_tok, fra_tok = eng.split(), fra.split()
     eng_maxlen = max(eng_maxlen, len(eng_tok))
     fra_maxlen = max(fra_maxlen, len(fra_tok))
     eng_tokens.update(eng_tok)
     fra_tokens.update(fra_tok)
-    
-print(f"Total English tokens : {len(eng_tokens)}")
-print(f"Total French tokens : {len(fra_tokens)}")
-print(f"Max English lengths : {eng_maxlen}")
-print(f"Max French lengths : {fra_maxlen}")
+print(f"Total English tokens: {len(eng_tokens)}")
+print(f"Total French tokens: {len(fra_tokens)}")
+print(f"Max English length: {eng_maxlen}")
+print(f"Max French length: {fra_maxlen}")
 print(f"{len(text_pairs)} total pairs")
 
 import matplotlib.pyplot as plt
@@ -147,87 +143,112 @@ plt.show()
 # =============================================================================
 from tensorflow.keras.layers import TextVectorization
 
+ 
 # Load normalized sentence pairs
 with open("text_pairs.pickle", "rb") as fp:
     text_pairs = pickle.load(fp)
-    
-# train-test-val split of randomized sentence pairs 
+ 
+# train-test-val split of randomized sentence pairs
 random.shuffle(text_pairs)
 n_val = int(0.15*len(text_pairs))
 n_train = len(text_pairs) - 2*n_val
 train_pairs = text_pairs[:n_train]
 val_pairs = text_pairs[n_train:n_train+n_val]
 test_pairs = text_pairs[n_train+n_val:]
-
+ 
 # Parameter determined after analyzing the input data
 vocab_size_en = 10000
 vocab_size_fr = 20000
 seq_length = 20
-
+ 
 # Create vectorizer
 eng_vectorizer = TextVectorization(
-    max_tokens = vocab_size_en,
+    max_tokens=vocab_size_en,
     standardize=None,
     split="whitespace",
     output_mode="int",
     output_sequence_length=seq_length,
-    )
-
+)
 fra_vectorizer = TextVectorization(
-    max_tokens = vocab_size_fr,
+    max_tokens=vocab_size_fr,
     standardize=None,
     split="whitespace",
     output_mode="int",
-    output_sequence_length=seq_length,
-    )
-
+    output_sequence_length=seq_length + 1
+)
+ 
 # train the vectorization layer using training dataset
 train_eng_texts = [pair[0] for pair in train_pairs]
 train_fra_texts = [pair[1] for pair in train_pairs]
 eng_vectorizer.adapt(train_eng_texts)
 fra_vectorizer.adapt(train_fra_texts)
-
+ 
 # save for subsequent steps
 with open("vectorize.pickle", "wb") as fp:
     data = {
         "train": train_pairs,
-        "val": val_pairs,
-        "test": test_pairs,
-        "engvec_config": eng_vectorizer.get_config(),
+        "val":   val_pairs,
+        "test":  test_pairs,
+        "engvec_config":  eng_vectorizer.get_config(),
         "engvec_weights": eng_vectorizer.get_weights(),
-        "fravec_config": fra_vectorizer.get_config(),
+        "fravec_config":  fra_vectorizer.get_config(),
         "fravec_weights": fra_vectorizer.get_weights(),
-        }
+    }
     pickle.dump(data, fp)
     
-# Load text data and vectorizer weights
-with open("victorize.pickle", "rb") as fp:
-    data = pickle.load(fp)
     
+ 
+# load text data and vectorizer weights
+with open("vectorize.pickle", "rb") as fp:
+    data = pickle.load(fp)
+ 
 train_pairs = data["train"]
 val_pairs = data["val"]
-test_pairs = data["test"] # Not used
-
+test_pairs = data["test"]   # not used
+ 
 eng_vectorizer = TextVectorization.from_config(data["engvec_config"])
-eng_vectorizer.set_weigths(data["engvec_weights"])
+eng_vectorizer.set_weights(data["engvec_weights"])
 fra_vectorizer = TextVectorization.from_config(data["fravec_config"])
-fra_vectorizer.set_weigths(data["fravec_weights"])
-
-# Set up dataset object
+fra_vectorizer.set_weights(data["fravec_weights"])
+ 
+# set up Dataset object
 def format_dataset(eng, fra):
+    """Take an English and a French sentence pair, convert into input and target.
+    The input is a dict with keys `encoder_inputs` and `decoder_inputs`, each
+    is a vector, corresponding to English and French sentences respectively.
+    The target is also vector of the French sentence, advanced by 1 token. All
+    vector are in the same length.
+ 
+    The output will be used for training the transformer model. In the model we
+    will create, the input tensors are named `encoder_inputs` and `decoder_inputs`
+    which should be matched to the keys in the dictionary for the source part
+    """
     eng = eng_vectorizer(eng)
     fra = fra_vectorizer(fra)
     source = {"encoder_inputs": eng,
               "decoder_inputs": fra[:, :-1]}
     target = fra[:, 1:]
     return (source, target)
-
+ 
 def make_dataset(pairs, batch_size=64):
-    
-    # Aggregate sentences using zip(*pairs)
+    """Create TensorFlow Dataset for the sentence pairs"""
+    # aggregate sentences using zip(*pairs)
     eng_texts, fra_texts = zip(*pairs)
+    # convert them into list, and then create tensors
     
-    # Convert them into, and then create tensors
     dataset = tf.data.Dataset.from_tensor_slices((list(eng_texts), list(fra_texts)))
-    return dataset.shuffle(2048).batch(batch_size).map(format_dataset) \
-                  .prefetch(16).cache()
+    return dataset.shuffle(2048) \
+                  .batch(batch_size).map(format_dataset).prefetch(16).cache()
+ 
+train_ds = make_dataset(train_pairs)
+val_ds = make_dataset(val_pairs)
+
+ 
+# test the dataset
+for inputs, targets in train_ds.take(1):
+    print(f'inputs["encoder_inputs"].shape: {inputs["encoder_inputs"].shape}')
+    print(f'inputs["encoder_inputs"][0]: {inputs["encoder_inputs"][0]}')
+    print(f'inputs["decoder_inputs"].shape: {inputs["decoder_inputs"].shape}')
+    print(f'inputs["decoder_inputs"][0]: {inputs["decoder_inputs"][0]}')
+    print(f"targets.shape: {targets.shape}")
+    print(f"targets[0]: {targets[0]}")
